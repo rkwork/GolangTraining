af17c1b2a298f03ce755a46f02c11d70e8ea433f
Raj Kiran Gade <rajkiran.gade@gmail.com>
2016-07-03 13:20:22 +0530

    modified 01_helloWorld
    Added new lines

--- 01_getting-started/01_helloWorld/main.go
+++ 01_getting-started/01_helloWorld/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// This is function main
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Hello World")
+	fmt.Printf("%d - %b - %x - %q", 65, 65, 65, 65)
+
 }
