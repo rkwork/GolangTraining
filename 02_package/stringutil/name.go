443140f185e02e57589f06792c154f085bf73452
Raj Kiran Gade <rajkiran.gade@gmail.com>
2016-07-15 11:45:40 +0530

    updated 02_package
    Updated 02_package folder and reverseTwo.go file

--- 02_package/stringutil/name.go
+++ 02_package/stringutil/name.go
@@ -2,3 +2,6 @@ package stringutil
 
 // MyName will be exported because it starts with a capital letter.
 var MyName = "Todd"
+
+// myName will not be exported because it starts with a lowercase letter.
+var myName = "Raj Kiran"
